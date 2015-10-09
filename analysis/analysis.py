bad38883b89f85804873632bf91b341b4a733820
smsharma <smsharma@princeton.edu>
2015-10-09 13:31:38 -0400

    Cut and data now work with each other

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -7,24 +7,46 @@ import os, sys
 
 
 class Data:
-    def __init__(self, input, display=True, lumi=20*1000):
-        self.data_ = pd.read_csv(input+".evt", sep=',',skipinitialspace=True, comment="#", index_col=('evt') ) 
-        self.obj_ = pd.read_csv(input+".obj", sep=',',skipinitialspace=True, comment="#", index_col=('evt','type','n') ) 
-        self.meta = pd.read_csv(input+".meta", sep=',',skipinitialspace=True, comment="#", index_col=None )
+    def __init__(self, input, display=True, lumi=20*1000, load_data=True):
+        if load_data:
+            if os.path.exists(input+".evt"): self.data_ = pd.read_csv(input+".evt", sep=',',skipinitialspace=True, comment="#", index_col=('evt') ) 
+            if os.path.exists(input+".obj"): self.obj_ = pd.read_csv(input+".obj", sep=',',skipinitialspace=True, comment="#", index_col=('evt','type','n') ) 
+            if os.path.exists(input+".meta"): self.meta = pd.read_csv(input+".meta", sep=',',skipinitialspace=True, comment="#", index_col=None )
         self.w_=np.asarray(self.meta.at[0,'cxn']*lumi/self.meta.at[0,'nevt'])
         
         self.data = self.data_
         self.obj = self.obj_
         self.w =np.repeat(self.w_, len(self.data.index))
+        self.cuts = Cuts()
         
-    def select(self, criterion):
-        self.data = self.data_.query(criterion)
+    def select(self, cuts):
+        query = ''
+        for cut in cuts._cuts:
+            if query == '':
+                query += cut
+            else: query += '& ' + cut 
+        if query != '': self.data = self.data_.query(query)
+        else: self.data = self.data_
         evt_list = list(self.data.index)
         self.obj = self.obj_.loc[evt_list]
         self.w =np.repeat(self.w_, len(self.data.index))
+        self.cuts = cuts
         
     def get_obj(self, type, n=1):
         return self.obj.xs((type, n), level=('type','n'))
+
+    def convert(self,pt, mass, eta, phi):
+        e = sqrt( (pt*cosh(eta))**2 + mass**2 )
+        px = pt*cos(phi)
+        py = pt*sin(phi)
+        pz = pt*sinh(eta)
+    
+        return np.asarray([px,py,pz,e]).T
+
+    def dR(self,dphi, deta=0):
+        dphi = abs(dphi % (2*pi))
+        dphi = dphi if dphi < pi else dphi-pi
+        return sqrt(dphi**2 + deta**2)
 
 # a class to keep track of cuts
 class Cuts:

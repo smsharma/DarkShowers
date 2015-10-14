78c878e9f3ec9c4c32351b562fcc4294076c9b60
Siddharth Mishra Sharma <smsharma@feynman.princeton.edu>
2015-10-13 22:07:23 -0400

    Added feynman scripts

---
+++ scripts/run_masses.py
@@ -0,0 +1,28 @@
+import os
+# import paramiko
+import numpy as np
+
+batch='''#!/bin/sh
+#PBS -j oe
+#PBS -o /home/smsharma/scratch/job.out
+#PBS -l nodes=1:ppn=1 -l cput=10:59:00
+#PBS -q hepheno
+#PBS -m abe
+#PBS -k oe
+#PBS -M smsharma@princeton.edu
+
+cd /group/hepheno/venv
+source bin/activate
+source /group/hepheno/smsharma/Dark_Showers/env.sh
+cd /group/hepheno/smsharma/Dark_Showers/gen
+source /group/hepheno/heptools/root/bin/thisroot.sh
+./jets_simple.exe -m tchannel -n 1000 -mphi '''
+
+mDMList = np.array([100,400,600,800,1000,2000,3000])
+for mDM in mDMList:
+    batchn = batch + str(mDM) + ' -o tchannel_mchitilde_' + str(mDM)
+    fname = "run_mphi_"+str(mDM)+".batch"
+    f=open(fname, "w")
+    f.write(batchn)
+    f.close()
+    os.system("qsub -l mem=16gb "+fname);
